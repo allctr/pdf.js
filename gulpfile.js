92c0dd3aba987d48cd63d05faf2e570d338a67db
Hassan Abdul Rehman <hassan.rehman@gmail.com>
2020-02-10 12:05:15 +0000

    prod mode ..allowing only public files

--- gulpfile.js
+++ gulpfile.js
@@ -1376,10 +1376,15 @@ gulp.task(
 
 gulp.task("server", function() {
   console.log();
-  console.log("### Starting local server");
 
   var WebServer = require("./test/webserver.js").WebServer;
   var server = new WebServer();
+  if (process.env.PDFJS_ENV == "production") {
+    console.log("### Starting production server");
+    server.root = "./build/generic"
+  }
+  else
+    console.log("### Starting local server");
   server.host = "0.0.0.0"
   server.port = (process.env.PORT || 8888);
   server.start();
